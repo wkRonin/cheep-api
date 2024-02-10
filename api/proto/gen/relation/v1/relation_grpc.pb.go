a5ec8c6365881815603610f5d8bf5391a35ee432
wkRonin <912370908@qq.com>
2024-02-10 21:59:11 +0800

    fix package name

--- api/proto/gen/relation/v1/relation_grpc.pb.go
+++ api/proto/gen/relation/v1/relation_grpc.pb.go
@@ -19,17 +19,17 @@ import (
 const _ = grpc.SupportPackageIsVersion7
 
 const (
-	FollowService_Follow_FullMethodName       = "/relation.v1.FollowService/Follow"
-	FollowService_CancelFollow_FullMethodName = "/relation.v1.FollowService/CancelFollow"
-	FollowService_GetFollowing_FullMethodName = "/relation.v1.FollowService/GetFollowing"
-	FollowService_GetFans_FullMethodName      = "/relation.v1.FollowService/GetFans"
-	FollowService_FollowInfo_FullMethodName   = "/relation.v1.FollowService/FollowInfo"
+	RelationService_Follow_FullMethodName       = "/relation.v1.RelationService/Follow"
+	RelationService_CancelFollow_FullMethodName = "/relation.v1.RelationService/CancelFollow"
+	RelationService_GetFollowing_FullMethodName = "/relation.v1.RelationService/GetFollowing"
+	RelationService_GetFans_FullMethodName      = "/relation.v1.RelationService/GetFans"
+	RelationService_FollowInfo_FullMethodName   = "/relation.v1.RelationService/FollowInfo"
 )
 
-// FollowServiceClient is the client API for FollowService service.
+// RelationServiceClient is the client API for RelationService service.
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
-type FollowServiceClient interface {
+type RelationServiceClient interface {
 	// 增删
 	Follow(ctx context.Context, in *FollowRequest, opts ...grpc.CallOption) (*FollowResponse, error)
 	CancelFollow(ctx context.Context, in *CancelFollowRequest, opts ...grpc.CallOption) (*CancelFollowResponse, error)
@@ -41,63 +41,63 @@ type FollowServiceClient interface {
 	FollowInfo(ctx context.Context, in *FollowInfoRequest, opts ...grpc.CallOption) (*FollowInfoResponse, error)
 }
 
-type followServiceClient struct {
+type relationServiceClient struct {
 	cc grpc.ClientConnInterface
 }
 
-func NewFollowServiceClient(cc grpc.ClientConnInterface) FollowServiceClient {
-	return &followServiceClient{cc}
+func NewRelationServiceClient(cc grpc.ClientConnInterface) RelationServiceClient {
+	return &relationServiceClient{cc}
 }
 
-func (c *followServiceClient) Follow(ctx context.Context, in *FollowRequest, opts ...grpc.CallOption) (*FollowResponse, error) {
+func (c *relationServiceClient) Follow(ctx context.Context, in *FollowRequest, opts ...grpc.CallOption) (*FollowResponse, error) {
 	out := new(FollowResponse)
-	err := c.cc.Invoke(ctx, FollowService_Follow_FullMethodName, in, out, opts...)
+	err := c.cc.Invoke(ctx, RelationService_Follow_FullMethodName, in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *followServiceClient) CancelFollow(ctx context.Context, in *CancelFollowRequest, opts ...grpc.CallOption) (*CancelFollowResponse, error) {
+func (c *relationServiceClient) CancelFollow(ctx context.Context, in *CancelFollowRequest, opts ...grpc.CallOption) (*CancelFollowResponse, error) {
 	out := new(CancelFollowResponse)
-	err := c.cc.Invoke(ctx, FollowService_CancelFollow_FullMethodName, in, out, opts...)
+	err := c.cc.Invoke(ctx, RelationService_CancelFollow_FullMethodName, in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *followServiceClient) GetFollowing(ctx context.Context, in *GetFollowingRequest, opts ...grpc.CallOption) (*GetFollowingResponse, error) {
+func (c *relationServiceClient) GetFollowing(ctx context.Context, in *GetFollowingRequest, opts ...grpc.CallOption) (*GetFollowingResponse, error) {
 	out := new(GetFollowingResponse)
-	err := c.cc.Invoke(ctx, FollowService_GetFollowing_FullMethodName, in, out, opts...)
+	err := c.cc.Invoke(ctx, RelationService_GetFollowing_FullMethodName, in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *followServiceClient) GetFans(ctx context.Context, in *GetFansRequest, opts ...grpc.CallOption) (*GetFansResponse, error) {
+func (c *relationServiceClient) GetFans(ctx context.Context, in *GetFansRequest, opts ...grpc.CallOption) (*GetFansResponse, error) {
 	out := new(GetFansResponse)
-	err := c.cc.Invoke(ctx, FollowService_GetFans_FullMethodName, in, out, opts...)
+	err := c.cc.Invoke(ctx, RelationService_GetFans_FullMethodName, in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *followServiceClient) FollowInfo(ctx context.Context, in *FollowInfoRequest, opts ...grpc.CallOption) (*FollowInfoResponse, error) {
+func (c *relationServiceClient) FollowInfo(ctx context.Context, in *FollowInfoRequest, opts ...grpc.CallOption) (*FollowInfoResponse, error) {
 	out := new(FollowInfoResponse)
-	err := c.cc.Invoke(ctx, FollowService_FollowInfo_FullMethodName, in, out, opts...)
+	err := c.cc.Invoke(ctx, RelationService_FollowInfo_FullMethodName, in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-// FollowServiceServer is the server API for FollowService service.
-// All implementations must embed UnimplementedFollowServiceServer
+// RelationServiceServer is the server API for RelationService service.
+// All implementations must embed UnimplementedRelationServiceServer
 // for forward compatibility
-type FollowServiceServer interface {
+type RelationServiceServer interface {
 	// 增删
 	Follow(context.Context, *FollowRequest) (*FollowResponse, error)
 	CancelFollow(context.Context, *CancelFollowRequest) (*CancelFollowResponse, error)
@@ -107,157 +107,157 @@ type FollowServiceServer interface {
 	GetFans(context.Context, *GetFansRequest) (*GetFansResponse, error)
 	// 获得某个人关注另外一个人的详细信息
 	FollowInfo(context.Context, *FollowInfoRequest) (*FollowInfoResponse, error)
-	mustEmbedUnimplementedFollowServiceServer()
+	mustEmbedUnimplementedRelationServiceServer()
 }
 
-// UnimplementedFollowServiceServer must be embedded to have forward compatible implementations.
-type UnimplementedFollowServiceServer struct {
+// UnimplementedRelationServiceServer must be embedded to have forward compatible implementations.
+type UnimplementedRelationServiceServer struct {
 }
 
-func (UnimplementedFollowServiceServer) Follow(context.Context, *FollowRequest) (*FollowResponse, error) {
+func (UnimplementedRelationServiceServer) Follow(context.Context, *FollowRequest) (*FollowResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Follow not implemented")
 }
-func (UnimplementedFollowServiceServer) CancelFollow(context.Context, *CancelFollowRequest) (*CancelFollowResponse, error) {
+func (UnimplementedRelationServiceServer) CancelFollow(context.Context, *CancelFollowRequest) (*CancelFollowResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method CancelFollow not implemented")
 }
-func (UnimplementedFollowServiceServer) GetFollowing(context.Context, *GetFollowingRequest) (*GetFollowingResponse, error) {
+func (UnimplementedRelationServiceServer) GetFollowing(context.Context, *GetFollowingRequest) (*GetFollowingResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetFollowing not implemented")
 }
-func (UnimplementedFollowServiceServer) GetFans(context.Context, *GetFansRequest) (*GetFansResponse, error) {
+func (UnimplementedRelationServiceServer) GetFans(context.Context, *GetFansRequest) (*GetFansResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetFans not implemented")
 }
-func (UnimplementedFollowServiceServer) FollowInfo(context.Context, *FollowInfoRequest) (*FollowInfoResponse, error) {
+func (UnimplementedRelationServiceServer) FollowInfo(context.Context, *FollowInfoRequest) (*FollowInfoResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method FollowInfo not implemented")
 }
-func (UnimplementedFollowServiceServer) mustEmbedUnimplementedFollowServiceServer() {}
+func (UnimplementedRelationServiceServer) mustEmbedUnimplementedRelationServiceServer() {}
 
-// UnsafeFollowServiceServer may be embedded to opt out of forward compatibility for this service.
-// Use of this interface is not recommended, as added methods to FollowServiceServer will
+// UnsafeRelationServiceServer may be embedded to opt out of forward compatibility for this service.
+// Use of this interface is not recommended, as added methods to RelationServiceServer will
 // result in compilation errors.
-type UnsafeFollowServiceServer interface {
-	mustEmbedUnimplementedFollowServiceServer()
+type UnsafeRelationServiceServer interface {
+	mustEmbedUnimplementedRelationServiceServer()
 }
 
-func RegisterFollowServiceServer(s grpc.ServiceRegistrar, srv FollowServiceServer) {
-	s.RegisterService(&FollowService_ServiceDesc, srv)
+func RegisterRelationServiceServer(s grpc.ServiceRegistrar, srv RelationServiceServer) {
+	s.RegisterService(&RelationService_ServiceDesc, srv)
 }
 
-func _FollowService_Follow_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _RelationService_Follow_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(FollowRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(FollowServiceServer).Follow(ctx, in)
+		return srv.(RelationServiceServer).Follow(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: FollowService_Follow_FullMethodName,
+		FullMethod: RelationService_Follow_FullMethodName,
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(FollowServiceServer).Follow(ctx, req.(*FollowRequest))
+		return srv.(RelationServiceServer).Follow(ctx, req.(*FollowRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _FollowService_CancelFollow_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _RelationService_CancelFollow_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(CancelFollowRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(FollowServiceServer).CancelFollow(ctx, in)
+		return srv.(RelationServiceServer).CancelFollow(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: FollowService_CancelFollow_FullMethodName,
+		FullMethod: RelationService_CancelFollow_FullMethodName,
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(FollowServiceServer).CancelFollow(ctx, req.(*CancelFollowRequest))
+		return srv.(RelationServiceServer).CancelFollow(ctx, req.(*CancelFollowRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _FollowService_GetFollowing_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _RelationService_GetFollowing_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(GetFollowingRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(FollowServiceServer).GetFollowing(ctx, in)
+		return srv.(RelationServiceServer).GetFollowing(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: FollowService_GetFollowing_FullMethodName,
+		FullMethod: RelationService_GetFollowing_FullMethodName,
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(FollowServiceServer).GetFollowing(ctx, req.(*GetFollowingRequest))
+		return srv.(RelationServiceServer).GetFollowing(ctx, req.(*GetFollowingRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _FollowService_GetFans_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _RelationService_GetFans_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(GetFansRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(FollowServiceServer).GetFans(ctx, in)
+		return srv.(RelationServiceServer).GetFans(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: FollowService_GetFans_FullMethodName,
+		FullMethod: RelationService_GetFans_FullMethodName,
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(FollowServiceServer).GetFans(ctx, req.(*GetFansRequest))
+		return srv.(RelationServiceServer).GetFans(ctx, req.(*GetFansRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _FollowService_FollowInfo_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _RelationService_FollowInfo_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(FollowInfoRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(FollowServiceServer).FollowInfo(ctx, in)
+		return srv.(RelationServiceServer).FollowInfo(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: FollowService_FollowInfo_FullMethodName,
+		FullMethod: RelationService_FollowInfo_FullMethodName,
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(FollowServiceServer).FollowInfo(ctx, req.(*FollowInfoRequest))
+		return srv.(RelationServiceServer).FollowInfo(ctx, req.(*FollowInfoRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-// FollowService_ServiceDesc is the grpc.ServiceDesc for FollowService service.
+// RelationService_ServiceDesc is the grpc.ServiceDesc for RelationService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
-var FollowService_ServiceDesc = grpc.ServiceDesc{
-	ServiceName: "relation.v1.FollowService",
-	HandlerType: (*FollowServiceServer)(nil),
+var RelationService_ServiceDesc = grpc.ServiceDesc{
+	ServiceName: "relation.v1.RelationService",
+	HandlerType: (*RelationServiceServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
 			MethodName: "Follow",
-			Handler:    _FollowService_Follow_Handler,
+			Handler:    _RelationService_Follow_Handler,
 		},
 		{
 			MethodName: "CancelFollow",
-			Handler:    _FollowService_CancelFollow_Handler,
+			Handler:    _RelationService_CancelFollow_Handler,
 		},
 		{
 			MethodName: "GetFollowing",
-			Handler:    _FollowService_GetFollowing_Handler,
+			Handler:    _RelationService_GetFollowing_Handler,
 		},
 		{
 			MethodName: "GetFans",
-			Handler:    _FollowService_GetFans_Handler,
+			Handler:    _RelationService_GetFans_Handler,
 		},
 		{
 			MethodName: "FollowInfo",
-			Handler:    _FollowService_FollowInfo_Handler,
+			Handler:    _RelationService_FollowInfo_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
